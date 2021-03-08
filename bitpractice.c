fffcd14d63bdced21fdcbed8e1b5d2cb6515fc24
Tarr Márton <tarr.marton1@gmail.com>
2021-03-08 19:09:57 +0100

    "Bugfixes"

--- bitpractice.c
+++ bitpractice.c
@@ -8,7 +8,7 @@
 #include <fcntl.h>
 
 
-struct pixel{
+/*struct pixel{
     unsigned char blue;
     unsigned char green;
     unsigned char red;
@@ -41,11 +41,11 @@ struct pixel *TestArray(int *NumCh){
     }
 
     return rgb_array;
-}
+}*/
 
-//char*
 
-char* Unwrap(struct pixel *Pbuff, int NumCh){
+//second
+char* Unwrap(char *Pbuff, int NumCh){
     char *letters = malloc(NumCh * sizeof(char) + 1);
 
     //Check for Failure
@@ -56,23 +56,25 @@ char* Unwrap(struct pixel *Pbuff, int NumCh){
 
     //set the mask
     int mask = 0b00000111;
-    int character = 0;
+    char character;
+    int j = 0;
 
-    //Decode the rgb elements from the struct
-    for (int i = 0; i < NumCh; i++)
+    //Decode the rgb elements from the array
+    for (int i = 0; i < NumCh * 3; i+=3)
     {
-        character = ((Pbuff[i].blue & mask)<<5) + ((Pbuff[i].green & mask)<<2) + (Pbuff[i].red & mask);
+        character = ((Pbuff[i] & mask)<<6) | ((Pbuff[i+1] & mask)<<3) | ((Pbuff[i + 2] & mask));
         character &= ~(1UL << 7);
-        letters[i] = (char)character;
+        letters[j] = character;
+        j++;
     }
 
     //terminate the string
-    letters[NumCh] = '\0';
+    letters[NumCh - 1] = '\0';
 
     return letters;
 }
 
-char* ReadPixels(char* f) //+ int* numCh
+char* ReadPixels(char* f, int* numCh)
 {
     int i;
     FILE* file = fopen(f, "rb");
@@ -81,66 +83,25 @@ char* ReadPixels(char* f) //+ int* numCh
     // read the 54-byte header
     fread(info, sizeof(unsigned char), 54, file); 
 
-    /*int fd;
-    fd = open(filename, O_RDWR);
-    read(fd, info, 54*sizeof(unsigned char));*/
+    int fd;
+    fd = open(f, O_RDWR);
+    read(fd, info, 54*sizeof(unsigned char));
 
     // extract image height and width from header
     int width = *(int*)&info[18];
     int height = *(int*)&info[22];
     int offset = *(int*)&info[10];
+    int text_length = *(int*)&info[6];
+
+    *numCh = text_length;
+
+    int padded_row =(width*3 + 3) & (~3); 
+    int size = padded_row * height;
+
+    char* data = malloc(size * sizeof(char));
     
-
-    printf("width: %d height:%d offset:%d\n", width, height, offset);
-
-    //int padded_row = (width*3 + 3) & (~3);
-
-    // allocate 3 bytes per pixel
-    //int size = 3 * width * height;
-    //unsigned char* data = malloc(size * sizeof(unsigned char));
-    int padded_row =(width*3 + 3) & (~3); //4 - ((width*3)%4);//
-    int foska = 4 - ((width*3)%4);
-    printf("padded: %d  foska: %d width: %d\n", padded_row, foska, width);
-    unsigned char tmp;
-    unsigned char* data;
-
-    int sum = 0;
-
-    for(int i = 0; i < 1; i++)
-    {
-        data = malloc(padded_row * sizeof(unsigned char));
-
-        //struct pixel *rgb_array = malloc(width * sizeof(struct pixel*));
-        fread(data, sizeof(unsigned char), padded_row, file);
-        //read(fd, data, padded_row * sizeof(unsigned char));
-        int i = 0;
-        int mask = 0b00000111;
-        for(int j = 0; j < width; j += 3)
-        {
-
-            /*int character = ((data[j] & mask)<<5) | ((data[j+1] & mask)<<2) | (data[j+2] & mask);
-            character &= ~(1UL << 7);
-            rgb_array[i].blue = data[j];
-            rgb_array[i].green = data[j+1];
-            rgb_array[i].red = data[j+2];
-            i++;*/
-
-            unsigned int character = ((data[j] & mask)<<6) | ((data[j+1] & mask)<<3) | ((data[j + 2] & mask));
-            character &= ~(1UL << 7);
-            /*if((char)character == '\n'){
-                character = ((data[j + 5] & mask)<<6) | ((data[j+ 4] & mask)<<3) | ((data[j + 3] & mask));
-                printf("%c", character);
-                character = ((data[j + 8] & mask)<<6) | ((data[j+7] & mask)<<3) | ((data[j + 6] & mask));
-                printf("%c", character);
-                character = ((data[j + 11] & mask)<<6) | ((data[j+10] & mask)<<3) | ((data[j + 9] & mask));
-                printf("%c", character);
-            }*/
-            printf("%c", character);
-
-        }
-    }
-
-    fclose(file);
+    read(fd, data, size * sizeof(char));
+    
     return data;
 }
 
@@ -148,21 +109,18 @@ char* ReadPixels(char* f) //+ int* numCh
 
 int main(){
 
-    srand(time(NULL));
 
-    int num = 3;
+    int num = 0;
 
     //Create Test Array, and decode it
-    struct pixel *array = TestArray(&num);
-    char *hidden_text = Unwrap(array, num);
+    ////struct pixel *array = TestArray(&num);
+    
 
-    char* fos = ReadPixels("cpu02.bmp");
+    char* pixel_array = ReadPixels("cpu02.bmp", &num);
+    char *hidden_text = Unwrap(pixel_array, num);
+    printf("%s", hidden_text);
 
-
-    //printf("%s", hidden_text);
-    //Free allocated memory
-    //free(hidden_text);
-    free(array);
+    free(pixel_array);
 
     return EXIT_SUCCESS;
 }
