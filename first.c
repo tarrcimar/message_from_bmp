9a8c42bf6d0f243cef61eea7ca2d0121e38d966c
Tarr Márton <tarr.marton1@gmail.com>
2021-03-28 21:10:25 +0200

    V4

--- first.c
+++ first.c
@@ -11,9 +11,16 @@
 #include <pwd.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
 #include "colors.h"
 
-#define VERSION "Version: 0.3 \nDate: 2021.03.15 \nAuthor: Tarr Marton"
+#define PORT 80
+
+#define VERSION "Version: 0.4 \nDate: 2021.03.28 \nAuthor: Tarr Marton"
 #define HELP "If a file-name is not specified as an arg, you will have to find it in a directory.\n\
 Typing the name of a directory works the same as 'cd <directory>.\n\
 Typing 'back' will act as 'cd..'.\n"
@@ -26,7 +33,7 @@ char* Unwrap(char *Pbuff, int NumCh){
     	fprintf(stderr, "No hidden text. The program will now exit!\n");
     	exit(1);
     }
-    char *letters = malloc(NumCh * sizeof(char) + 1);
+    char *letters = malloc((NumCh - 1) * sizeof(char)); // így nincs benne \n
     //Check for Failure
     if(letters == NULL){
         fprintf(stderr, "Memory could not be allocated! Program will exit..");
@@ -39,7 +46,7 @@ char* Unwrap(char *Pbuff, int NumCh){
     int j = 0;
     
     //Decode the rgb elements from the array
-    for (int i = 0; i < NumCh * 3; i+=3)
+    for (int i = 0; i < (NumCh - 1) * 3; i+=3) //NumCh ha kell \n
     {
         character = ((Pbuff[i] & mask)<<6) | ((Pbuff[i+1] & mask)<<3) | ((Pbuff[i + 2] & mask));
         letters[j] = character;
@@ -47,7 +54,7 @@ char* Unwrap(char *Pbuff, int NumCh){
     }
 
     //terminate the string
-    letters[NumCh-1] = '\n';
+    //letters[NumCh-1] = '\n';
     
     return letters;
 }
@@ -85,9 +92,10 @@ int BrowseForOpen(){
 	struct dirent* entry;
 	struct stat inode;
 	char tmp[PATH_MAX];
-	char pw[PATH_MAX] = "/";
+	char tmp_pw[PATH_MAX];
 	int regularfile;
 	char* extension;
+	char* previous;
 	
 	
 	dir = opendir("/home");
@@ -112,12 +120,21 @@ int BrowseForOpen(){
 		//Scan the path that should be opened
 		printf("\nWhat do you want to open?\n");
 		scanf("%s", tmp);
+		getcwd(tmp_pw, sizeof(tmp_pw)); //get the current directory
+		char* ptr = strrchr(tmp_pw, '/'); //last occurence of '/' in string
+		char pw_dir[(ptr-tmp_pw)+1];
+		
+		for(int i = 0; i<(ptr-tmp_pw)+1; i++){
+			pw_dir[i] = tmp_pw[i]; // copy to pw_dir until the last '/', this is the previous directory
+		}
+		
 		//if back is written, then execute "cd .."
 		if(strcmp(tmp, "back") == 0){
-			chdir(pw);
+			//chdir(pw);
+			printf("%s:%ld\n",pw_dir,strlen(pw_dir));
+			chdir(pw_dir);
 		}
 		else{
-			getcwd(pw, sizeof(pw));
 			while(chdir(tmp) != 0){
 				stat(tmp, &path_stat);
 				//if not a directory, check if it's a regular file, if yes then open and return
@@ -137,15 +154,70 @@ int BrowseForOpen(){
 	return 0;
 }
 
-/*extension = strrchr(tmp, '.');
-    				if(strcmp(extension + 1, "bmp") == 0){	
-						printf("%s\n",extension + 1);*/
+
+int Post(char* neptunID, char* message, int NumCh){
+	time_t t = time(NULL);
+  	struct tm tm = *localtime(&t);
+  	printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	
+
+	int portno = 80;
+	char* host = "irh.inf.unideb.hu";
+	char* message_fmt = "POST /~vargai/post.php HTTP/1.1\r\n"
+						"Host: %s\r\n"
+						"Content-Length: %d\r\n"
+						"Content-Type: application/x-www-form-urlencoded\r\n\r\n"
+						"NeptunID=%s&PostedText=%s\r\n"; // full post payload
+	
+	
+	struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, received, total;
+    char messageform[4096],response[4096], time[1024];
+
+	
+    /* fill in the parameters */
+    sprintf(time, "%d.%02d.%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    int length = strlen(time) + strlen(neptunID) + NumCh + 1; // length of date string, neptunID and the message
+    sprintf(messageform,message_fmt,host,length,neptunID,message); 
+    
+    printf("%s\nlength:%d\ntime:%s", messageform, length, time);
+
+    /* create the socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) fprintf(stderr, "ERROR opening socket");
+
+    /* lookup the ip address */
+    server = gethostbyname(host);
+    if (server == NULL) fprintf(stderr, "ERROR: no such host");
+
+    /* fill in the structure */
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_addr.s_addr = inet_addr("193.6.135.148");
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    /* connect the socket */
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        fprintf(stderr, "ERROR connecting");
+
+  	send(sockfd, messageform, sizeof(messageform), 0);
+  	recv(sockfd, response, sizeof(response), 0);
+  	close(sockfd);
+  	
+  	if(strstr(response, "The message has been received.") != NULL) return 0;
+  	else return 2;
+  	
+}
+
 
 int main(int argc, char* argv[]){
     char c;
     int file = 0;
     char* extension;
     int num = 0;
+    int response;
     
     if(argc == 2){
     	extension = strrchr(argv[1], '.');
@@ -179,7 +251,9 @@ int main(int argc, char* argv[]){
         }
         char* pixel_array = ReadPixels(file, &num);
     	char* hidden_text = Unwrap(pixel_array, num);
-		printf("%s\n",hidden_text);
+		response = Post("G8R7ZQ", hidden_text, num);
+		if(response == 0) printf("Post successful! Text sent.\n");
+		else if(response != 0) fprintf(stderr, "Text could not be posted!\n");
 		free(pixel_array);
 		free(hidden_text);
     }
